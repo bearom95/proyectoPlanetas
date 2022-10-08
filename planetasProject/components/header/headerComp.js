49b4336aa6ae9512bdcde1415f3b82dee704b5df
bearom95 <bearom95@gmail.com>
2022-10-08 17:34:26 +0200

    Después de cambiar letras

--- planetasProject/components/header/headerComp.js
+++ planetasProject/components/header/headerComp.js
@@ -4,9 +4,11 @@
 export const initHeader = ()=> {
 const navbar =   `
 <nav>
-<h1>Planets</h1>
-<button class="btn" id="btnSmall">Small Planets</button>
-<button class="btn" id="btnBig">Big Planets</button>
+    <h1>Planets</h1>
+        <div>
+            <button class="btn" id="btnSmall">Small Planets</button>
+            <button class="btn" id="btnBig">Big Planets</button>
+        </div>
 </nav>
 `
 return navbar
